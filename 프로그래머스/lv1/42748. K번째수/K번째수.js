43927504c1fe8bdb7124cdba0f036d4bbf227c19
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-09-27 13:51:48 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let list = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(list[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
