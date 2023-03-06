aaee4eb4fcf7b37501feb2cebf18ad2a6265cb52
Jack <96723716+Exist95@users.noreply.github.com>
2023-03-06 17:12:06 +0900

    [level 0] Title: 가위 바위 보, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,11 @@
+function solution(rsp) {
+    var answer = '';
+    for(let i=0; i<rsp.length; i++){
+        if(rsp[i] === "0"){
+            answer+='5'
+        } else if (rsp[i] === '2') {
+            answer+='0'
+        } else answer +='2'
+    }
+    return answer;
+}
