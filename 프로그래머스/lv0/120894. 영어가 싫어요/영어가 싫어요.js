b613a1ffccced47c66073ad3c727fee130a768c5
Jack <96723716+Exist95@users.noreply.github.com>
2023-03-23 15:13:36 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let answer = '';
+    let obj = {zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9}
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+    return +num;
+}
