695973c2b5bfbd7e8a8a5a9fa9082414e6b4cddf
노종열 <96723716+Exist95@users.noreply.github.com>
2024-03-18 00:43:58 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,6 @@
+function solution(a, b, c) {
+    if(a === b && b === c && a === c) return (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3)
+    if(a === b || b === c || a=== c) return (a + b + c) * (a**2 + b**2 + c**2)
+    return a+b+c
+}
+
